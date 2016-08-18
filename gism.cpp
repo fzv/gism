69fb4b6a635920b803a8c0a6f92d2f9e24e3e602
fzv <fatima.vayani@kcl.ac.uk>
2016-08-18 15:29:17 +0100

    printing T works

--- gism.cpp
+++ gism.cpp
@@ -22,7 +22,6 @@ if (inputFile.is_open()){
 	if (inputFile.good()){
 		while (getline(inputFile, line)){
 			lines.push_back(line);
-			//std::cout << line << std::endl;
 		}
 	}
 	inputFile.close();
@@ -39,46 +38,36 @@ std::vector<std::string> tempVector;
 std::stringstream tempString;
 
 for (int i=0; i<t.length(); i++){ //loop through text string
-	std::cout << "just read " << t[i] << std::endl; //print current symbol
 	if (t[i]=='{'){ //if new pos in T
 		tempVector.clear(); //create vector to hold letters in current pos in T
 		tempString.str("");
 		tempString.clear();
-		std::cout << "printing tempVector\n";
-		for (auto i=0; i< tempVector.size(); i++) std::cout << tempVector[i] << std::endl;
-		std::cout << "\nprinting tempString\n";
-		std::cout << tempString.str() << std::endl;
 	} else if (t[i]=='}'){ //if reach end of current pos in T
 		tempVector.push_back(tempString.str()); //add current s to tempVector
 		T.push_back(tempVector); //fill current pos in T with tempVector
-		std::cout << "printing tempVector\n";
-		for (auto i=0; i< tempVector.size(); i++) std::cout << tempVector[i] << std::endl;
-		std::cout << "\nprinting tempString\n";
-		std::cout << tempString.str() << std::endl;
 	} else if (t[i]==','){
 		tempVector.push_back(tempString.str()); //add current s to tempVector
 		tempString.str("");
 		tempString.clear();
-		std::cout << "printing tempString\n";
-		std::cout << tempString.str() << std::endl;
-		std::cout << "printing tempVector\n";
-		for (auto i=0; i< tempVector.size(); i++) std::cout << tempVector[i] << std::endl;
-		std::cout << std::endl;
 	} else {
-		std::cout << "printing tempString\n";
 		tempString << t[i];
-		std::cout << tempString.str() << std::endl;
 	}
-	std::cout << std::endl << std::endl;
 }
 tempVector.clear();
 tempString.str("");
 tempString.clear();
 
-std::cout << std::endl << std::endl << "trying to print T properly" << std::endl << std::endl;
-for (auto i=0; i<T.size(); i++){
-	tempVector = T[i];
+//Print input sequences
+std::cout << "string T:" << std::endl;
+for (std::list<std::vector<std::string>>::iterator i=T.begin(); i!=T.end(); i++){
+	tempVector = *i;
+	for (std::vector<std::string>::iterator j=tempVector.begin(); j!=tempVector.end(); j++){
+		std::cout << *j << " ";
+	}
+	std::cout << std::endl;
 }
+std::cout << "\nstring P:" << std::endl;
+std::cout << P << std::endl;
 
 
 
