278f09ef5a00ee07e3c0bd05207a5d04d50dacc5
fzv <fatima.vayani@kcl.ac.uk>
2016-10-11 16:19:15 +0100

    abc

---
+++ synthesiseData.cpp
@@ -0,0 +1,18 @@
+#include <iostream> //cout,endl
+#include <string> //string,
+#include <vector> //vector,push_back
+#include <fstream> //ifstream,is_open,good,getline,close
+#include <sstream> //
+
+/*
+
+This script takes parameters...
+- n : number of positions in text T
+- d% : percentage of positions in text T which are degenerate
+- S_max : maximum size of set at any position T[i] i.e. maximum number of S_j
+- L_max : upper bound on length of any string S_j in T[i]
+...and makes use of...
+- DNA : a vector holding the DNA alphabet
+...in order to...
+1. 
+*/
