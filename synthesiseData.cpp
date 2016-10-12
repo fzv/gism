e50a0f2cd156faa29976b063c442404a9c5b9950
fzv <fatima.vayani@kcl.ac.uk>
2016-10-12 04:23:45 +0100

    working on it

--- synthesiseData.cpp
+++ synthesiseData.cpp
@@ -1,3 +1,5 @@
+// g++ synthesiseData.cpp -o synthesise -std=c++11
+
 #include <iostream> //cout,endl
 #include <string> //string,
 #include <vector> //vector,push_back
@@ -5,14 +7,7 @@
 #include <sstream> //
 #include <ctime> //
 #include <cstdlib> //
-
-int main()
-{
-
-std::srand(std::time(NULL));
-
-return 0;
-}
+#include <math.h> //ceil
 
 /*
 
@@ -39,3 +34,78 @@ pickRandomBase():
 1. randomNumberGenerator for int x = {0, 1, 2, 3}
 2. return DNA[x]
 */
+
+std::string pickRandomBase(std::vector<std::string> *dna);
+void createSet();
+
+int main(int argc, char* argv[])
+{
+
+/* user given parameters */
+//...-n: number of positions in text T
+int n = atoi(argv[2]); 
+std::cout << "n is " << n << std::endl;
+//...- d : percentage of positions in text T which are degenerate i.e. represent indels
+int d = atoi(argv[4]); 
+std::cout << "d is " << d << "%" << std::endl;
+//...- Smax : maximum size of set at any position T[i] i.e. maximum number of S_j
+int Smax = atoi(argv[6]); 
+std::cout << "Smax is " << Smax << std::endl;
+//...- Lmax : upper bound on length of any string S_j in T[i]
+int Lmax = atoi(argv[8]); 
+std::cout << "Lmax is " << Lmax << std::endl;
+
+/* declare other variables */
+// a vector of strings holding the DNA alphabet
+std::vector<std::string> dna = {"A", "G", "C", "T"};
+// int vector D of random positions in T (totalling d% of T) which should be sets instead of singleton chars
+std::vector<int> D;
+
+/* build D */
+//convert % into number dprime out of n
+int dprime = n / 100;
+std::cout << "dprime is " << dprime << std::endl;
+dprime = dprime * d;
+std::cout << "dprime is " << dprime << std::endl;
+dprime = ceil(dprime);
+std::cout << "dprime is " << dprime << std::endl;
+//for 0 to dprime-1
+//...push back random number between 0 to n-1 to D
+for (int i=0; i<dprime; i++){
+	D.push_back( std::rand() % n );
+}
+//print D
+std::cout << "printing vector D" << std::endl;
+for (std::vector<int>::iterator it=D.begin(); it!=D.end(); it++){
+	std::cout << *it << " ";
+}
+std::cout << std::endl;
+
+
+
+
+//set seed for pickRandomBase()
+std::srand(std::time(NULL));
+//test adding char to stringstream
+std::stringstream s;
+s << pickRandomBase(&dna);
+std::cout << s.str() << std::endl;
+
+
+
+return 0;
+}
+
+void createSet()
+{
+//set random s <= Smax (range begins from 2)
+///int s = ( std::rand() % (Smax-2) ) + 2;
+
+}
+
+std::string pickRandomBase(std::vector<std::string> *dna)
+{
+int number = ( std::rand() % 4 );
+std::cout << (*dna)[number] << std::endl;
+return (*dna)[number];
+}
